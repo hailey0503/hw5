5a28868a560499af16ee965e08967b0fa1f1818c
Hailey Pong <haileypong@berkeley.edu>
2020-06-26 05:13:50 +0000

    Finished my first CS 162 assignment

--- hw0/limits.c
+++ hw0/limits.c
@@ -5,9 +5,9 @@ int main() {
     struct rlimit lim;
     getrlimit(RLIMIT_STACK, &lim);
     printf("stack size: %ld\n", lim.rlim_cur);
-    getrlimit(RLIMIT_CORE, &lim);
+    getrlimit(RLIMIT_NPROC, &lim);
     printf("process limit: %ld\n", lim.rlim_cur);
-    getrlimit(RLIMIT_AS, &lim);
+    getrlimit(RLIMIT_NOFILE, &lim);
     printf("max file descriptors: %ld\n", lim.rlim_cur);
     return 0;
 }
